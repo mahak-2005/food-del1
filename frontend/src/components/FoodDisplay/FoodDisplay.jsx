17d459a1777c33c565a0e6a9503c7f348c31e536
mahak-2005 <mahakgupta177@gmail,com>
2026-03-28 14:25:26 +0530

    update search option

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,20 +3,36 @@ import './FoodDisplay.css'
 import FoodItem from '../FoodItem/FoodItem'
 import { StoreContext } from '../../Context/StoreContext'
 
-const FoodDisplay = ({category}) => {
+const FoodDisplay = ({ category }) => {
 
-  const {food_list} = useContext(StoreContext);
+  const { food_list, search } = useContext(StoreContext);
+
+  // ✅ FILTER LOGIC
+  const filteredList = food_list.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
+
       <div className='food-display-list'>
-        {food_list.map((item)=>{
-          if (category==="All" || category===item.category) {
-            return <FoodItem key={item._id} image={item.image} name={item.name} desc={item.description} price={item.price} id={item._id}/>
+        {filteredList.map((item) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={item._id}
+                image={item.image}
+                name={item.name}
+                desc={item.description}
+                price={item.price}
+                id={item._id}
+              />
+            )
           }
         })}
       </div>
+
     </div>
   )
 }
