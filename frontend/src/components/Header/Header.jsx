16ac053e86a8efd902b0b3547a2bd7f8627c63ed
mahak-2005 <mahakgupta177@gmail,com>
2026-03-29 20:09:56 +0530

    changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
 
 const Header = () => {
+
+    const images = [
+        "/header_img.png",
+        "/header_img1.jpg",
+        "/header_img2.jpg"
+    ]
+
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prev) => (prev + 1) % images.length)
+        }, 3000)
+
+        return () => clearInterval(interval)
+    }, [])
+
+    // 🔥 FUNCTION ADD KIYA
+    const handleScroll = () => {
+        const section = document.getElementById("menu")
+        if(section){
+            section.scrollIntoView({ behavior: "smooth" })
+        }
+    }
+
     return (
-        <div className='header'>
+        <div 
+            className='header'
+            style={{ backgroundImage: `url(${images[index]})` }}
+        >
             <div className='header-contents'>
                 <h2>Order your favourite food here</h2>
-                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-                <button>View Menu</button>
+                <p>
+                    Choose from a diverse menu featuring a delicious variety of dishes.
+                </p>
+
+                {/* 🔥 BUTTON FIX */}
+                <button onClick={handleScroll}>
+                    View Menu
+                </button>
+
             </div>
         </div>
     )
